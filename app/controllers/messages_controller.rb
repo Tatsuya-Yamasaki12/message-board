e906bb91f5434f56ce550f2fb7bdf6ba2b1d987d
tatsuya.yamasaki <ythn.0308@gmail.com>
2020-11-10 18:23:17 +0000

    add colum

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,7 +53,7 @@ class MessagesController < ApplicationController
     
     #strong Parameter
     def message_params
-        params.require(:message).permit(:content)
+        params.require(:message).permit(:content, :title)
     end
 
 end
